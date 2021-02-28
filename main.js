1ce801a386e8ac51a31b373adcec077a650712c9
emadPoursina <emad.puursina@gmail.com>
2021-02-28 07:21:18 +0330

    change main.js

--- main.js
+++ main.js
@@ -1,53 +1,66 @@
+/**
+ * This script will generate an i3bar
+ */
+
 const { exec } = require("child_process");
 
 function shell(command) {
 	return new Promise((resolve, reject) => {
-		exec(command, (err, result) => { resolve(result); reject(new Error(err)); });
+		exec(command, (err, result) => {
+			if(err) {
+				return reject(new Error(err));
+			}
+			return resolve(result);
+		});
 	});
 }
 
-async function battryGenerator() {
-	let battryO = await shell("acpi");
-	battryO = battryO.split(",");
-	battryO[0] = battryO[0].slice(11);
-	battryO[1] = battryO[1].trim().substring(0, battryO[1].indexOf("%"));
-	battryO[2] = battryO[2].trim();
+console.log("{ \"version\": 1 }");
 
-	const battry = {
-		status: battryO[0],
-		percentage: battryO[1],
-		remainingTime: battryO[2],
-	}
-
-	const final = {
-		name: "Battry",
-		full_text: `Battry: ${battry.percentage} : ${battry.status}`,
-	};
-
-	if(battry.percentage.substring(0, 2) < 15){
-		final.urgent = true;
-	}else if(battry.status == "Charging") {
-		final.color = "#2cfc03";
-	}
-
-	return final;
-}
-
-async function dateGenerator() {
-	let date = await shell("date");
-	date = date.trim();
-	const final = {name: "Date", full_text: date};
-
-	return final;
-}
-
-console.log('{ "version": 1 }');
 console.log("[");
+
 console.log("[],");
 
-setInterval(async () => {
-	const battry = await battryGenerator();
-	const date = await dateGenerator();
+async function barGenerator() {
+	const ip = await shell("hostname -I");
+	const date = await shell("date");
+	const acpi = await shell("acpi");
+	const sizeStorage = await shell("df -H / | awk '{print $4}' | grep G");
+	const cpuUsage = await shell("mpstat  | awk '{print $4}' | awk 'NR==4'");
+	hourlyReminder();
 
-	console.log(`[${JSON.stringify(battry)}, ${JSON.stringify(date)}],`);
-}, 1000);
+	console.log(
+		`[
+			{
+				"name": "localIpAddressV4",
+				"full_text": "iPv4: ${ip.trim()}"
+			},
+			{
+				"name": "cpuUsage",
+				"full_text": "CPU: ${cpuUsage.trim()}"
+			},
+			{
+				"name": "battryStatus",
+				"full_text": ": ${acpi.trim()}"
+			},
+			{
+				"name": "sizeStorage",
+				"full_text": "SDA: ${sizeStorage.trim()}"
+			},
+			{
+				"name": "time",
+				"full_text": "${date.trim()}"
+			}
+		],`
+	);
+}
+
+function hourlyReminder(clock) {
+	const d = new Date();
+	//console.log(d.getMinutes());
+	if(d.getMinutes() === '32'&& d.getSeconds() === '20'){
+		const d = shell("notify-send -u critical \"Write your job report\"");
+	}
+}
+
+setInterval(barGenerator, 1000);
