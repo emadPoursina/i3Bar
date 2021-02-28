8bb83212feb071add8d6af0ed99954d762a73717
emadPoursina <emad.puursina@gmail.com>
2021-02-28 11:47:27 +0330

    add battryGenerator function

--- src/battry.js
+++ src/battry.js
@@ -7,9 +7,24 @@ function acpi() {
         console.log(stderr);
         reject(code);
       }else{
+        const battery = batteryGenerator(output);
       }
     });
   })
+}
+
+/**
+ * Transfare acpi output to a javascript object
+ * @param {String} acpiOutput 
+ */
+function batteryGenerator(acpiOutput) {
+  const info = acpiOutput.substring(acpiOutput.indexOf(':') + 2).split(', ');
+  return {
+    name: acpiOutput.substring(0, acpiOutput.indexOf(':')),
+    status: info[0],
+    remainingPercentage: info[1],
+    remainingTime: info[2],
+  };
 }
 
 module.exports = acpi();
