4d21656ba103e1a48a5400f178e703ca5c5927d9
emadPoursina <emad.puursina@gmail.com>
2021-03-03 10:21:31 +0330

    process i3bar click event data

--- src/click.js
+++ src/click.js
@@ -1,6 +1,22 @@
 const readline = require('readline');
+const logFile = require('fs').createWriteStream('/home/emad/NodeTown/i3Bar/log');
 
 const rl = readline.createInterface({
   input: process.stdin,
   terminal: false
+});
+let flag = false;
+
+rl.on('line', function(line){
+  try {
+    if(flag) {
+      if(line[0] === ',')
+        line = line.slice(1);
+      const b = JSON.parse(line);
+    }else {
+      flag = true;
+    }
+  } catch (error) {
+    logFile.write(error.toString());
+  }
 });
